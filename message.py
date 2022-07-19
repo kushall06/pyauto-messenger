7ea024ddac4f60bb32c9d69ed8d850ce7a9fa102
Anonymous Noble <83326666+kushall06@users.noreply.github.com>
2022-07-19 12:55:54 +0545

    Add files via upload

---
+++ message.py
@@ -0,0 +1,15 @@
+Python To Automate Messaging Apps
+Author:Anonymous Noble
+import pyautogui
+import time
+
+time.sleep(5)
+
+text='Your Text Here'
+
+while True:
+    pyautogui.typewrite(text)
+
+    time.sleep(1)
+    pyautogui.press('enter')
+    
